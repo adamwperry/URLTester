5d9c3fee67899ce4a34ad564cd5b89d73c793985
ryanjgross <ryanjgross@gmail.com>
2019-03-30 22:21:27 -0400

    More work on the unit tests and small code changes in the main project

--- URLTester/Parsers/CSVParser.cs
+++ URLTester/Parsers/CSVParser.cs
@@ -19,45 +19,46 @@ namespace Parsers
 
                 var newObject = new T();
                 var listNewObject = new List<T>();
-                var readFile = new StreamReader(File.OpenRead(filePath));
-                int lineCount = 1; //start at 1 to skip header
-                var getHeaders = readFile.ReadLine().Split(','); //Read headers
-                // List to store products.
-                while (!readFile.EndOfStream)
+                using (var readFile = new StreamReader(File.OpenRead(filePath)))
                 {
-                    var line = readFile.ReadLine(); //read each line
-                    //and split each line to get the values of the respective headers
-                    var delimiter = new char[','];
-                    var values = line.Split(',');
-                    int i = 0;
-                    foreach (String val in values)
+                    int lineCount = 1; //start at 1 to skip header
+                    var getHeaders = readFile.ReadLine().Split(','); //Read headers
+                                                                     // List to store products.
+                    while (!readFile.EndOfStream)
                     {
-                        //Get the header
-                        var currHeader = getHeaders[i].ToString();
-                        //find the object property -- note I am removing spaces and ToUpper and Trimming the file column headers
-                        var ojbProperty =
-                            newObject.GetType()
-                                .GetProperties()
-                                .Where(t => t.Name.ToUpper() == currHeader.Replace(" ", "").ToUpper().Trim())
-                                .FirstOrDefault();
-                        try
+                        var line = readFile.ReadLine(); //read each line
+                                                        //and split each line to get the values of the respective headers
+                        var delimiter = new char[','];
+                        var values = line.Split(',');
+                        int i = 0;
+                        foreach (String val in values)
                         {
-                            //try to cast the value to the current object property using reflection
-                            ojbProperty.SetValue(newObject, Convert.ChangeType(val, ojbProperty.PropertyType));
-                        }
-                        catch (Exception)
-                        {
-                            errorMessages.Add(new ErrorMessage(String.Format("An error occurred on line {0} property ({1}) value ({2}) is invalid. This property will be skipped.", lineCount.ToString(), ojbProperty.Name, val)));
-                        }
+                            //Get the header
+                            var currHeader = getHeaders[i].ToString();
+                            //find the object property -- note I am removing spaces and ToUpper and Trimming the file column headers
+                            var ojbProperty =
+                                newObject.GetType()
+                                    .GetProperties()
+                                    .Where(t => t.Name.ToUpper() == currHeader.Replace(" ", "").ToUpper().Trim())
+                                    .FirstOrDefault();
+                            try
+                            {
+                                //try to cast the value to the current object property using reflection
+                                ojbProperty.SetValue(newObject, Convert.ChangeType(val, ojbProperty.PropertyType));
+                            }
+                            catch (Exception)
+                            {
+                                errorMessages.Add(new ErrorMessage(String.Format("An error occurred on line {0} property ({1}) value ({2}) is invalid. This property will be skipped.", lineCount.ToString(), ojbProperty.Name, val)));
+                            }
 
-                        i++;
+                            i++;
+                        }
+                        //add new obj to list and reset
+                        listNewObject.Add(newObject);
+                        newObject = new T();
+                        lineCount++;
                     }
-                    //add new obj to list and reset
-                    listNewObject.Add(newObject);
-                    newObject = new T();
-                    lineCount++;
                 }
-
 
                 return listNewObject;
             }
