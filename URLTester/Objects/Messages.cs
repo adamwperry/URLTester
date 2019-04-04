19a0ebb74e6d4e415c7468b957c2c068f84f7a66
Adam Perry <adamwperry@gmail.com>
2019-04-04 00:40:28 -0400

    updated program.cs main output message (#11)
    Added a messages class for common message.
    
    Converted the program.cs main function to use this new class and take advantage of the OutputManger class.
    
    Fixed an issue with the hepl message.
    
    removed system.io from the list of classes used.

---
+++ URLTester/Objects/Messages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLTester.Objects
+{
+    /// <summary>
+    /// Common output messages.
+    /// </summary>
+    public static class Messages
+    {
+        public const string AppliationVersion = "URLTester version 1.2";
+        public const string Ellipsis = "...";
+        public const string LoadingFile = "Loading File" + Ellipsis;
+        public const string Running = "Running" + Ellipsis;
+        public const string Results = "Results" + Ellipsis;
+
+    }
+}
